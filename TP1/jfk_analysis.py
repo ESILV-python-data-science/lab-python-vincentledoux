bc3ae6972db1155f74d3bf8fd1695454400d581a
vincentledoux <vincen.ledoux@gmail.com>
2017-10-31 15:48:50 +0100

    ex2
    exercice 2

--- TP1/jfk_analysis.py
+++ TP1/jfk_analysis.py
@@ -2,13 +2,36 @@ import csv
 
 file = open("jfkrelease-2017-dce65d0ec70a54d5744de17d280f3ad2.csv")
 
-numField=len(file.readline().split(";"))
-for f in file:
+page = 0
+pageCount = 0
+count = 0
+pageMax = 0
+pageMin = 0
 
-        if numField == len(f.split(";")):
+numField = len(file.readline().split(";"))
+for f in file:
+        count = count + 1
+        tab = f.split(";")
+        page = int(tab[11])
+        pageCount = pageCount + page
+
+        if page > pageMax:
+            pageMax = page
+        if count == 1:
+            pageMin = page
+        if page < pageMin:
+            pageMin = page
+
+        if numField == len(tab):
             print("ok  line:  " + f)
         else:
             print("error")
+
+print(pageCount/count)
+print("max : " + pageMax + " min : " + pageMin)
+
+
+
 
 
 
