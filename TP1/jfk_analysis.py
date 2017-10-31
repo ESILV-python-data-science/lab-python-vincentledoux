c9586fa5d3179aaa7d0f39c896104692760468b5
vincentledoux <vincen.ledoux@gmail.com>
2017-10-31 15:23:47 +0100

    field equality
    test of field equality

--- TP1/jfk_analysis.py
+++ TP1/jfk_analysis.py
@@ -1,1 +1,17 @@
-***test***
+import csv
+
+file = open("jfkrelease-2017-dce65d0ec70a54d5744de17d280f3ad2.csv")
+
+numField=len(file.readline().split(";"))
+for f in file:
+
+        if numField == len(f.split(";")):
+            print("ok  line:  " + f)
+        else:
+            print("error")
+
+
+
+
+
+
