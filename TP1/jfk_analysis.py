3d4f8146fe582691e3aeb9ef1d8ce0587179b05c
vincentledoux <vincen.ledoux@gmail.com>
2017-11-03 19:36:46 +0100

    CountNoPage
    Count of the cases with no page

--- TP1/jfk_analysis.py
+++ TP1/jfk_analysis.py
@@ -6,29 +6,38 @@ page = 0
 pageCount = 0
 count = 0
 pageMax = 0
-pageMin = 0
+pageMin = 100
+countNoPage = 0;
 
 numField = len(file.readline().split(";"))
 for f in file:
-        count = count + 1
+
         tab = f.split(";")
-        page = int(tab[11])
-        pageCount = pageCount + page
 
-        if page > pageMax:
-            pageMax = page
-        if count == 1:
-            pageMin = page
-        if page < pageMin:
-            pageMin = page
+        try:
+            count = count + 1
+            page = int(tab[11])
+            pageCount = pageCount + page
 
-        if numField == len(tab):
-            print("ok  line:  " + f)
-        else:
-            print("error")
+            if page > pageMax:
+                pageMax = page
+            if count == 1:
+                pageMin = page
+            if page < pageMin:
+                pageMin = page
+
+            if numField == len(tab):
+                print("ok  line:  " + f)
+            else:
+                print("error")
+        except ValueError:
+
+            countNoPage += 1
+
 
 print(pageCount/count)
-print("max : " + pageMax + " min : " + pageMin)
+print("max : " + pageMax.__str__() + " min : " + pageMin.__str__())
+print("Count no page : " + countNoPage.__str__())
 
 
 
