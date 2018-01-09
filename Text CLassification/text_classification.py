3c10bcfc63ae4db10e9e53d01f98fc491da2e493
vincentledoux <vincen.ledoux@gmail.com>
2018-01-09 16:35:29 +0100

    quest 2 of text_classification
    quest_2 done

--- Text CLassification/text_classification.py
+++ Text CLassification/text_classification.py
@@ -7,6 +7,7 @@ import sys
 import pickle
 
 import clf as clf
+from sklearn.feature_extraction.text import CountVectorizer
 from tqdm import tqdm
 import pandas as pd
 import seaborn as sns
@@ -29,7 +30,7 @@ if __name__ == "__main__":
     input_group.add_argument('--articles-list',help='file containing the text path and text class, one per line, comma separated')
     args = parser.parse_args()
 
-    #read the doc
+    #quest 1
     df = pd.read_csv(args.articles_list, header=0, sep='\t')
     df.dropna(axis=0, how='all')
     print(df.size)
@@ -40,9 +41,18 @@ if __name__ == "__main__":
     sns.countplot(df['category'])
     plt.show()
 
-
-
-
+    #quest 2
+    X_train, X_temp, Y_train, Y_temp= train_test_split(df["text"], df["category"], test_size=0.4)
+    X_test, X_dev, Y_test, Y_dev = train_test_split(X_temp, Y_temp, test_size= 0.5)
+    vectorizer = CountVectorizer(max_features=1000)
+    vectorizer.fit(X_train)
+    X_train_counts = vectorizer.transform(X_train)
+    X_test_counts = vectorizer.transform(X_test)
+    print(X_train.size)
+    print(X_dev.size)
+    print(X_test.size)
+    print(Y_train.size)
+    print(X_train_counts)
 
 else:
     sys.exit()
